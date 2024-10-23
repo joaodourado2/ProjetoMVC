357a85800849b981896c6e02bfb5f3b4ddb2f713
joaodourado2 <87993902+joaodourado2@users.noreply.github.com>
2024-10-22 21:55:35 -0300

    Configuracao de Menu

--- src/main/java/com/projetomvc/aplicativo/produto/controller/ProdutoController.java
+++ src/main/java/com/projetomvc/aplicativo/produto/controller/ProdutoController.java
@@ -5,17 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.projetomvc.aplicativo.produto.model.Produto;
 
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
 
-	private Produto protudoTeste = new Produto();
 	
 	@GetMapping
 	public String produto(Model model) {
-		model.addAttribute("prTeste", protudoTeste.getNomeProduto());
+        model.addAttribute("title", "Tela de Produto");
 	    return "produto/produto";
 	}
 }
