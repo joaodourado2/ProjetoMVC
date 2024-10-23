36adca27f89c59b795c34e6f8449125af5b29df3
joaodourado2 <87993902+joaodourado2@users.noreply.github.com>
2024-10-22 21:21:24 -0300

    novas configuracoes de login

--- src/main/java/com/projetomvc/aplicativo/login/controller/LoginController.java
+++ src/main/java/com/projetomvc/aplicativo/login/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.projetomvc.aplicativo.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.projetomvc.aplicativo.login.model.User;
 
 @Controller
 @RequestMapping("/")
@@ -19,7 +25,18 @@ public class LoginController {
     }
 	
     @GetMapping("login")
-    public String login() {
+    public String login(Model model) {
+    	model.addAttribute("user", new User());
         return "login/login"; 
+    }
+
+    @PostMapping("login")
+    public String validateLogin(@ModelAttribute User userForm, RedirectAttributes redirectAttribute){
+        if ("teste".equals(userForm.getUserName())){
+            return "produto/produto";
+        }else{
+            redirectAttribute.addFlashAttribute("error","Usuário Inválido!");
+            return "redirect:/login";
+        }
     }
 }
